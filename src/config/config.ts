10e40813050ce1ffac712cefe498acf5a1c9009c
YossiGreen <>
2020-03-23 12:42:30 +0200

    - added pre-config.

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,16 +1,18 @@
 import DeviceInfo from 'react-native-device-info';
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import { onError } from '../services/ErrorService';
-import DefaultConfig from './DefaultConfig.json';
+import DefaultConfig from './default_config.json';
 import { Config } from '../types';
 
+// @ts-ignore
 const env: 'com.hamagen.dev'|'com.hamagen.qa'|'com.hamagen' = DeviceInfo.getBundleId();
 
 let config: Config = DefaultConfig[env];
 
 export const initConfig = async () => new Promise(async (resolve) => {
   try {
-    const { data } = await axios.get(`https://matrixdemos.blob.core.windows.net/mabar/config.json?r=${Math.random()}`, { headers: { 'Content-Type': 'application/json;charset=utf-8' } });
+    const res: AxiosResponse = await axios.get(`https://gisweb.azureedge.net/get_config.json?r=${Math.random()}`, { headers: { 'Content-Type': 'application/json;charset=utf-8' } });
+    const { data } = await axios.get(`${res.data[env]}?r=${Math.random()}`, { headers: { 'Content-Type': 'application/json;charset=utf-8' } });
 
     config = data[env];
     resolve();
