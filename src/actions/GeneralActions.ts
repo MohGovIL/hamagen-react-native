3d94bbfb6cb800de8599c5234cc62350be6a2def
YossiGreen <>
2020-03-23 11:12:24 +0200

    - done.

--- src/actions/GeneralActions.ts
+++ src/actions/GeneralActions.ts
@@ -1,9 +1,10 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-community/async-storage';
+import DeviceInfo from 'react-native-device-info';
 import { onError } from '../services/ErrorService';
 import config from '../config/config';
 import { TOGGLE_LOADER, TOGGLE_WEBVIEW, SHOW_FORCE_UPDATE, SHOW_FORCE_TERMS } from '../constants/ActionTypes';
-import { CURRENT_VERSION, CURRENT_TERMS_VERSION, IS_IOS } from '../constants/Constants';
+import { CURRENT_TERMS_VERSION, IS_IOS } from '../constants/Constants';
 
 export const toggleLoader = (isShow: boolean) => (dispatch: any) => dispatch({ type: TOGGLE_LOADER, payload: { isShow } });
 
@@ -11,7 +12,7 @@ export const toggleWebview = (isShow: boolean, usageType:string) => (dispatch: a
 
 export const checkForceUpdate = () => async (dispatch: any) => {
   try {
-    const { data: { ios, android, terms } } = await axios.get(`${config.versionsUrl}?r=${Math.random()}`, { headers: { 'Content-Type': 'application/json;charset=utf-8' } });
+    const { data: { ios, android, shouldForceIOS, shouldForceAndroid, terms } } = await axios.get(`${config.versionsUrl}?r=${Math.random()}`, { headers: { 'Content-Type': 'application/json;charset=utf-8' } });
 
     const termsVersion = JSON.parse(await AsyncStorage.getItem(CURRENT_TERMS_VERSION) || '0');
 
@@ -21,13 +22,11 @@ export const checkForceUpdate = () => async (dispatch: any) => {
       return dispatch({ type: SHOW_FORCE_TERMS, payload: { terms } });
     }
 
-    const currentVersion = IS_IOS ? ios : android;
+    const storeVersion = IS_IOS ? ios : android;
+    const appVersion = DeviceInfo.getVersion();
+    const shouldForce = IS_IOS ? shouldForceIOS : shouldForceAndroid;
 
-    const appVersion = JSON.parse(await AsyncStorage.getItem(CURRENT_VERSION) || '0');
-
-    if (!appVersion) {
-      await AsyncStorage.setItem(CURRENT_VERSION, JSON.stringify(currentVersion));
-    } else if ((appVersion !== 0) && (appVersion < currentVersion)) {
+    if ((appVersion !== storeVersion) && shouldForce) {
       dispatch({ type: SHOW_FORCE_UPDATE });
     }
   } catch (error) {
