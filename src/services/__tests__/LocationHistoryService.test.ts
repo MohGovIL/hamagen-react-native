681371e3063a028cc0aee49224896969aa1a6990
Sagiv Stekolshik <sagivstekolshik@Sagivs-MacBook-Pro.local>
2020-04-16 16:13:08 +0300

    add LocationHistoryService test start

---
+++ src/services/__tests__/LocationHistoryService.test.ts
@@ -0,0 +1,24 @@
+import { onError } from '../ErrorService';
+import * as db from '../../database/Database';
+import {getLoadingHTML} from '../LocationHistoryService'
+describe('LocationHistoryService', () => {
+
+    describe('getLoadingHTML', () => {
+        test('Is valid HTML', () => {
+            const loadedHTML = getLoadingHTML()
+            
+        })
+    })
+
+    describe('getLastNrDaysKmlUrls', () => {
+
+    })
+
+    describe('kmlToGeoJson', () => {
+
+    })
+
+    describe('insertToSampleDB', () => {
+
+    })
+})
