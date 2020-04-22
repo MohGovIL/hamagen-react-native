0e7c257b5e70b216c5b8f12db744650c295f1d08
YossiGreen <>
2020-04-22 14:35:07 +0300

    no message

--- src/actions/LocaleActions.ts
+++ src/actions/LocaleActions.ts
@@ -15,7 +15,7 @@ export const initLocale = () => async (dispatch: any) => {
 
     await AsyncStorage.setItem(CURRENT_LOCALE, activeLocale);
 
-    const data: LocaleData = await downloadAndVerifySigning(`${config().stringsUrl}frfr`);
+    const data: LocaleData = await downloadAndVerifySigning(config().stringsUrl);
 
     const { languages, notificationData, externalUrls } = data;
 
