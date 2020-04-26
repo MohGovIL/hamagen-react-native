1cd388468807de317ef6f8f85779a409a7155ba7
SagivOnoApps <sagivstekolshik@Sagivs-MacBook-Pro.local>
2020-04-26 11:18:29 +0300

    map modal properties

--- src/components/Main/MapModal.tsx
+++ src/components/Main/MapModal.tsx
@@ -1,5 +1,5 @@
 import React, { useMemo } from 'react';
-import { Modal ,View, StyleSheet} from 'react-native';
+import { Modal, View, StyleSheet } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import { useSelector, useDispatch } from 'react-redux';
 import moment from 'moment';
@@ -11,7 +11,7 @@ import { Store, GeneralReducer, LocaleReducer } from '../../types';
 const MapModal = () => {
   const dispatch = useDispatch();
   const {
-    showMap: { visible, region, properties: { Place, fromTime } },
+    showMap: { visible, region, properties },
   } = useSelector<Store, GeneralReducer>(state => state.general);
   const {
     strings: {
@@ -20,18 +20,17 @@ const MapModal = () => {
   } = useSelector<Store, LocaleReducer>(state => state.locale);
   
   
-  
-  const [date,hour] = useMemo(() => {
-    const time = moment(fromTime)
-    return [time.format('DD.MM.YY'),time.format('DD.MM.YY')]
-  }, [fromTime])
+  const [date, hour] = useMemo(() => {
+    const time = moment(properties?.fromTime);
+    return [time.format('DD.MM.YY'), time.format('DD.MM.YY')];
+  }, [properties?.fromTime]);
 
   return (
     <Modal visible={visible} animationType="slide">
       <HeaderButton type="close" onPress={() => dispatch({ type: HIDE_MAP_MODAL })} />
       <View style={styles.headerContainer}>
-        <Icon source={require('../../assets/main/exposuresSmall.png')} width={27} height={17} customStyles={styles.headerIcon}/>
-        <Text bold style={styles.place}>{Place}</Text>
+        <Icon source={require('../../assets/main/exposuresSmall.png')} width={27} height={17} customStyles={styles.headerIcon} />
+        <Text bold style={styles.place}>{properties?.Place}</Text>
         <Text style={styles.aroundTime}>{`${inDate} ${date} ${fromHour} ${hour}`}</Text>
       </View>
       <MapView
@@ -51,13 +50,13 @@ const MapModal = () => {
 
 const styles = StyleSheet.create({
   headerContainer: { height: SCREEN_HEIGHT * 0.22, justifyContent: 'center', alignItems: 'center', paddingTop: PADDING_TOP(51), marginBottom: IS_SMALL_SCREEN ? 28 : 0 },
-  headerIcon: {marginBottom: 8},
-  place: {fontSize: 16},
-  aroundTime: {fontSize: 14,marginTop: 10},
+  headerIcon: { marginBottom: 8 },
+  place: { fontSize: 16 },
+  aroundTime: { fontSize: 14, marginTop: 10 },
   mapStyle: {
     width: SCREEN_WIDTH,
     height: SCREEN_HEIGHT * 0.78,
   }
-})
+});
 
 export default MapModal;
