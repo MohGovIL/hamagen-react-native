64b48698734dab5618cdec197f6a508b7bf6be2d
SagivOnoApps <sagiv.s@onoapps.com>
2020-05-04 19:44:18 +0300

    seperate share link from share messege

--- src/components/Main/ScanHomeHeader.tsx
+++ src/components/Main/ScanHomeHeader.tsx
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { View, StyleSheet, ImageBackground, Share } from 'react-native';
 import { TouchableOpacity, Icon } from '../common';
 import { onError } from '../../services/ErrorService';
-import { Strings } from '../../locale/LocaleData';
+import { ExternalUrls,Strings, Languages } from '../../locale/LocaleData';
 import { HIT_SLOP, PADDING_TOP, SCREEN_HEIGHT, SCREEN_WIDTH } from '../../constants/Constants';
 
 interface ScanHomeHeaderProps {
   isRTL: boolean,
   strings: Strings,
+  externalUrls: ExternalUrls,
+  locale: string,
+  languages: Languages,
   openDrawer(): void
 }
 
-const ScanHomeHeader = ({ isRTL, strings: { scanHome: { share: { message, title, androidTitle } } }, openDrawer }: ScanHomeHeaderProps) => {
+const ScanHomeHeader = ({ isRTL,languages,locale,externalUrls,strings: { scanHome: { share: { message, title, androidTitle } } }, openDrawer }: ScanHomeHeaderProps) => {
+  const messageAndUrl = useMemo(()=> {
+    const relevantLocale: string = Object.keys(languages.short).includes(locale) ? locale : 'he';
+    return `${message}\n${externalUrls?.shareMessage?.[relevantLocale] ?? ''}`
+  }, [locale])
+
+
   const onShare = async () => {
     try {
-      await Share.share({ message, title }, { dialogTitle: androidTitle, subject: title});
+      await Share.share({ message: messageAndUrl, title }, { dialogTitle: androidTitle, subject: title});
     } catch (error) {
       onError({ error });
     }
