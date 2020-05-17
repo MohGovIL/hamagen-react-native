c5aeda1a93285e7c23ddb244a170fddea8848630
SagivOnoApps <sagiv.s@onoapps.com>
2020-05-17 19:23:37 +0300

    fix purging after 14 days

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -164,8 +164,9 @@ const Loading : FunctionComponent<Props> = (
       }
 
       const dbSick = new IntersectionSickDatabase();
-   
+      await dbSick.purgeIntersectionSickTable(moment().subtract(2, 'week').unix() * 1000)
       const exposures = await dbSick.listAllRecords();
+      console.log(exposures);
       
       
       await store().dispatch(setExposures(exposures.map((exposure: any) => ({ properties: { ...exposure } }))));
